d8dfe2cc878ffb5e04b29701d4c67847adf78aa8
chcjswo <chcjswo@gmail.com>
2017-01-12 01:21:55 +0900

    아까 커밋을 못했네 다시 한번

--- app.js
+++ app.js
@@ -3,16 +3,32 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var flash = require('connect-flash');
+var expressValidator = require('express-validator');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+// MongoDB 데이터베이스 접속하기
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/humor');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection errors: '));
+db.once('open', function callback () {
+	console.log('Successfully connected to MongoDB');
+});
+
 var app = express();
+
+app.locals.moment = require('moment');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,26 +37,84 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(errorHandler);
+
+// Express Session
+app.use(session({
+	secret: 'secret',
+	saveUninitialized: true,
+	resave: true
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value) {
+		var namespace = param.split('.')
+		, root    = namespace.shift()
+		, formParam = root;
+
+		while(namespace.length) {
+			formParam += '[' + namespace.shift() + ']';
+		}
+		return {
+			param : formParam,
+			msg   : msg,
+			value : value
+		};
+	}
+}));
+
+// Flash Messages
+app.use(flash());
+app.use(function (req, res, next) {
+	res.locals.messages = require('express-messages')(req, res);
+	next();
+});
 
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// catch 404 and forward to errors handler
+app.use(function(req, res) {
+	return res.render('errors/404');
 });
 
-// error handler
+// errors handlers
+
+// development errors handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('errors/errors', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+// production errors handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	res.status(err.status || 500);
+	res.render('errors/error', {
+		message: err.message,
+		error: {}
+	});
 });
+
+
+function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render('errors/errors', { error: err });
+}
+
 
 module.exports = app;
